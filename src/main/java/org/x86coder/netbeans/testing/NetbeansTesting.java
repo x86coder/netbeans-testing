4324b5b9bed35e41d6229ce41c5f38f6aafa6558
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-06-27 14:20:39 -0600

    project scaffolding

---
+++ src/main/java/org/x86coder/netbeans/testing/NetbeansTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.x86coder.netbeans.testing;
+
+/**
+ *
+ * @author Jaime
+ */
+public class NetbeansTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
