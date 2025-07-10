f9fa107f25272c2edb9cef7f4f534fd04835c8e7
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-07-09 18:38:14 -0600

    changes to namespace

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # netbeans-testing
 Java example framework with TestNG and Cucumber
+
+## Installation
+- Java: openjdk 17.0.2 2022-01-18
+- OS: Windows 10
+- IDE: Apache NetBeans IDE 25
+
+## Dependencies
+- cucumber-jvm
+- cucumber-junit
+- selenium
---
+++ src/main/java/qa/x86coder/netbeans/testing/NetbeansTesting.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package qa.x86coder.netbeans.testing;
+
+/**
+ *
+ * @author Amed Reyes
+ */
+public class NetbeansTesting {
+
+    public static void main(String[] args) {
+        System.out.println("> To run [feature files] build project or use: mvn test");
+    }
+}
